78271c1c6e71e7e2a6049fde38a028278a740648
happlesful <Swislar1108@gmail.com>
2025-07-09 21:31:21 +0800

    Add: Complete map of all current data (25/7/9)

---
+++ maps/index.js
@@ -0,0 +1,7 @@
+import musicMappings from "./musicMap.json" with { type: "json" };
+import emblemMappings from "./emblemMap.json" with { type: "json" };
+import albumCoverMappings from "./albumCoverMap.json" with { type: "json" };
+import artistMappings from "./artistMap.json" with { type: "json" };
+import musicNameMappings from "./musicNameMap.json" with { type: "json" };
+
+export { musicMappings, emblemMappings, albumCoverMappings, artistMappings,  musicNameMappings };
